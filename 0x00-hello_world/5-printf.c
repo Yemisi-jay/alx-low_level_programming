2d1befc2790b8c12c84acfa0bc28bccbbdbd3076
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-13 07:24:06 -0700

    program that prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function is constant ate the beginning of a code
+ *
+ * Return: this will return 0 when the code is executed
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
