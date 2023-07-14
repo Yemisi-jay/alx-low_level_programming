40687ef85b9541fa0a9de006ee8d3f5e8e0b0afc
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-14 14:52:19 -0700

    print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is constant in the code
+ *
+ * Return: this will return 0 when the code is executed
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+
+}
