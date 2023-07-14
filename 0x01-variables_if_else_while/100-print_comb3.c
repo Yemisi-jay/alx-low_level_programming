859bbbd28e9299579a7c41a1bab40d5ab65608d9
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-14 16:35:10 -0700

    print combinations of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,15 @@ int main(void)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (j = 0 ; j <= 9 ; j++)
 	{
-		for (j = 0 ; j <= 9 ; j++)
+		for (i = 0 ; i <= 9 ; i++)
 		{
-			if (!((i == j) || (j > i)))
+			if (!((j == i) || (i > j)))
 			{
-				putchar(i + '0');
 				putchar(j + '0');
-				if (!(i == 9 && j == 8))
+				putchar(i + '0');
+				if (!(j == 9 && i == 8))
 				{
 					putchar(',');
 					putchar(' ');
