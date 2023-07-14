7874d9bba7d5136569304131aa6e91d8b80c001e
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-14 13:54:55 -0700

    print A-Z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this the the main function
+ *
+ * Return: this should return 0 when the code runs sucessfully
+ */
+
+int main(void){
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return(0);
+}
