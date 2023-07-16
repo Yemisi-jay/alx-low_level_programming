07be2324cc9f2d1cd720c91475b01279945a5cbd
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-16 12:36:00 -0700

    print possoble combinations of digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,8 +11,8 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0 ; a <= 99 ; a++)
-		for (b = a ; b <= 99 ; b++)
+	for (a = 0 ; a < 100 ; a++)
+		for (b = a ; b < 100 ; b++)
 		{
 			putchar('0' + (a / 10));
 			putchar('0' + (a % 10));
