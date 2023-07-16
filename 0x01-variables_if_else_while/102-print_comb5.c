c4b15fdff178825f3bdf7e289adc04aaa5aaeba0
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-16 14:05:28 -0700

    print all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,35 @@
 
 int main(void)
 {
-	int a;
-	int b;
+	int tens_1, ones_1, tens_2, ones_2;
 
-	for (a = 0 ; a < 100 ; a++)
-		for (b = a ; b < 100 ; b++)
+	for (tens_1 = 0; tens_1 < 10; tens_1++)
+	{
+		for (ones_1 = 0; ones_1 < 10; ones_1++)
 		{
-			putchar('0' + (a / 10));
-			putchar('0' + (a % 10));
-			putchar(' ');
-			putchar('0' + (b / 10));
-			putchar('0' + (b % 10));
-
-			if (a != 99 || b != 99)
+			for (tens_2 = tens_1; tens_2 < 10; tens_2++)
 			{
-				putchar(',');
-				putchar(' ');
+				int start = (tens_1 == tens_2) ? ones_1 + 1 : 0;
+
+				for (ones_2 = start; ones_2 < 10; ones_2++)
+				{
+					putchar(tens_1 + '0');
+					putchar(ones_1 + '0');
+					putchar(' ');
+					putchar(tens_2 + '0');
+					putchar(ones_2 + '0');
+
+					if (tens_1 != 9 || ones_1 != 8 || tens_2 != 9 || ones_2 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
+	}
+
 	putchar('\n');
+
 	return (0);
 }
