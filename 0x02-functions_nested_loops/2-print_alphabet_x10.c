85207a6ac35a77c2bb7007de8ae398d1fe036aab
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-18 07:19:38 -0700

    funtion that checks for lowercase character

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - print all alphabet x10 times in a roll
+ *
+ * Return: return void
+ */
+
+void print_alphabet_x10(void)
+{
+char alphabet;
+int i;
+while (i < 10)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+i++;
+}
+
+}
