f25ced4695bb89cefaaf2882f9382b605bb5a656
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-19 07:10:40 -0700

    first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,21 +7,21 @@
 
 int main(void)
 {
-        unsigned long int num1 = 1;
-        unsigned long int num2 = 2;
-        unsigned long int sum;
-        unsigned long int i;
+	unsigned long int num1 = 1;
+	unsigned long int num2 = 2;
+	unsigned long int sum;
+	unsigned long int i;
 
-        printf("%lu, ", num1);
-        for (i = 1 ; i < 98 ; i++)
-        {
-                printf("%lu", num2);
-                sum = num1 + num2;
-                num1 = num2;
-                num2 = sum;
-                if (i != 97)
-                        printf(", ");
-        }
-        printf("\n");
-        return (0);
+	printf("%lu, ", num1);
+	for (i = 1 ; i < 98 ; i++)
+	{
+		printf("%lu", num2);
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+		if (i != 97)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
 }
