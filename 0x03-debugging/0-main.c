bbba693d964da23a7b31405bbc6216164d6f5048
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-07-19 05:32:42 -0700

    0x03-debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - this tst for positive or negative
+ *
+ * Return: return 0 when sucessful
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
