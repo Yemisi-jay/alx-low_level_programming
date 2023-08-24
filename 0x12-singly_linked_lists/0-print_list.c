88f1f0ab0de4a998ce9b27a5255b62f21f6c66cd
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-08-24 09:39:31 -0700

    free list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t k = 0;
 
 	while (h)
 	{
@@ -19,9 +19,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		k++;
 	}
 
-	return (s);
+	return (k);
 }
 
