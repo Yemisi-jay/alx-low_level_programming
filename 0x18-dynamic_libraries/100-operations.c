e31741d27f785f8c9dbfc0a209a311e68f6b339b
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-12-13 05:43:33 -0800

    operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/**
+* add- function that adds two inputs
+* @a: first input to add
+* @b: second input to add
+* Return: return the value of a and b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - function that subtracts two inputs
+* @a: first input to subtract
+* @b: second input to subtract
+* Return: return the value of a and b
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - function that multiplies two inputs
+* @a: first input to multiply
+* @b: second input to multiply
+* Return: return the value of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - function that divides two inputs
+* @a: first input to divide
+* @b: second input to divide
+* Return: return the value of a and b
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - function that returns the modulos two inputs
+* @a: first input modulos
+* @b: second input for modulos
+* Return: return the value of a and b
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
